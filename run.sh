fb0561a532ea01fb8f5393f4d7e54a241ece2cd5
“zhuly59” <2574391830@qq.com>
2024-12-21 14:03:26 +0800

    update yaml2

--- run.sh
+++ run.sh
@@ -23,26 +23,66 @@ python examples/run_knowedit_llama2.py \
 #     --datatype='zsre' \
 #     --metrics_save_dir=./results/gpt2-xl/ICE
 
-
+# python examples/run_knowedit_llama2.py \
+#     --editing_method=ICE \
+#     --hparams_dir=./hparams/ICE/gpt2-xl.yaml \
+#     --data_dir=./data/wikidata_recent.json \
+#     --datatype='recent' \
+#     --metrics_save_dir=./results/gpt2/ICE
 
 # python examples/run_knowedit_llama2.py \
 #     --editing_method=ICE \
-#     --hparams_dir=./hparams/ICE/gpt-j-6b.yaml \
-#     --datatype='zsre' \
-#     --metrics_save_dir=./results/gpt-j-6b/ICE\
-#     --data_dir=./data/zsre_train_10000.json \
+#     --hparams_dir=./hparams/ICE/gpt2-xl.yaml \
+#     --data_dir=./data/wikidata_counterfact.json \
+#     --datatype='counterfact' \
+#     --metrics_save_dir=./results/gpt2/ICE
 
 
 # python examples/run_knowedit_llama2.py \
 #     --editing_method=ICE \
 #     --hparams_dir=./hparams/ICE/llama-7b.yaml \
-#     --data_dir=./data/zsre_train_10000.json \
+#     --data_dir=./data/zsre-origin.json \
 #     --datatype='zsre' \
 #     --metrics_save_dir=./results/llama-7b/ICE
 
 # python examples/run_knowedit_llama2.py \
 #     --editing_method=ICE \
-#     --hparams_dir=./hparams/ICE/llama-8b.yaml \
+#     --hparams_dir=./hparams/ICE/llama-7b.yaml \
+#     --data_dir=./data/zsre.json \
 #     --datatype='zsre' \
-#     --metrics_save_dir=./results/llama-8b/ICE \
+#     --metrics_save_dir=./results/llama-7b/ICE
+
+# python examples/run_knowedit_llama2.py \
+#     --editing_method=ICE \
+#     --hparams_dir=./hparams/ICE/llama-7b.yaml \
+#     --data_dir=./data/zsre-001.json \
+#     --datatype='zsre' \
+#     --metrics_save_dir=./results/llama-7b/ICE
+
+
+# python examples/run_knowedit_llama2.py \
+#     --editing_method=ICE \
+#     --hparams_dir=./hparams/ICE/llama-7b.yaml \
+#     --data_dir=./data/zsre_eval_10.json \
+#     --datatype='zsre' \
+#     --metrics_save_dir=./results/llama-7b/ICE
+
+# python examples/run_knowedit_llama2.py \
+#     --editing_method=ICE \
+#     --hparams_dir=./hparams/ICE/llama-7b.yaml \
+#     --data_dir=./data/zsre_eval_1000.json \
+#     --datatype='zsre' \
+#     --metrics_save_dir=./results/Llama-2-7b-hf/ICE
+
+python examples/run_knowedit_llama2.py \
+    --editing_method=ICE \
+    --hparams_dir=./hparams/ICE/gpt-j-6b.yaml \
+    --datatype='zsre' \
+    --metrics_save_dir=./results/gpt-j-6b/ICE\
+    --data_dir=./data/zsre_train_10000.json \
+
+
+# python examples/run_knowedit_llama2.py \
+#     --editing_method=ICE \
+#     --hparams_dir=./hparams/ICE/llama-7b.yaml \
 #     --data_dir=./data/zsre_train_10000.json \
